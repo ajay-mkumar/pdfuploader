f100acbcd8ddbb7c8958caae483f141a4c37cf2a
ajay-mkumar <ajaymkumar172@gmail.com>
2021-06-05 18:03:58 +0530

    host

--- pdf/settings.py
+++ pdf/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'avc',
         'USER': 'postgres',
         'PASSWORD': '0911',
-        'HOST': 'localhost',
+        'HOST': '127:0:0:1',
         'PORT':'5432'
     }
 }
