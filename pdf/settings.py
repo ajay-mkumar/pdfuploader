cf6d55ca3401ebcf3087b2a6a9c014be036fe948
ajay-mkumar <ajaymkumar172@gmail.com>
2021-06-10 16:52:58 +0530

    debug

--- pdf/settings.py
+++ pdf/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eb6$-4&h3xtoa8#f11xw2=xg_-h=lw#oj4y-_z%*@@fs_f4d*8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['pdfuploader-avc.herokuapp.com','127:0:0:1']
 
