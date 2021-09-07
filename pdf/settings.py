152c4205a65c914cf5c14474db6b4c8ef0c29e22
ajay-mkumar <ajaymkumar172@gmail.com>
2021-09-07 15:44:44 +0530

    db changes

--- pdf/settings.py
+++ pdf/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'pdf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': '2UBcml331R',
-        'USER': '2UBcml331R',
-        'PASSWORD': 'ExxyzKxTfD',
-        'HOST': 'remotemysql.com',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
